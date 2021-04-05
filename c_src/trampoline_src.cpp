bf5dc182c5d95d456786362d599646ba7608bc0d
shravanrn@gmail.com <shravanrn@gmail.com>
2021-04-04 22:04:42 -0700

    Move heavy trampoline code into own repo

--- c_src/trampoline_src.cpp
+++
@@ -1,8 +0,0 @@
-
-#include "ctx_save_trampoline.hpp"
-
-thread_local TransitionContext* saved_transition_context = nullptr;
-
-TransitionContext* get_saved_transition_context() {
-  return saved_transition_context;
-}
